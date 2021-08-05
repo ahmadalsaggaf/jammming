9593be5db19148d1f185f59d6d8ed059f03e395f
Ahmed Alsaqaf <ahmadalsaggaf30@gmail.com>
2021-08-05 18:17:26 -0400

    Create Static Components

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -1,5 +1,8 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 
 class App extends React.Component {
   render() {
@@ -12,7 +15,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults />
-            <Playlist />
+            {/* <Playlist /> */}
           </div>
         </div>
       </div>
